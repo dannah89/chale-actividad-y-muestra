5fcf5bba1c9ea312b68a14d8889639b4bd330ee9
danna henao <sucorreo>
2021-10-27 18:08:54 -0500

    concha

---
+++ js/dom.js
@@ -0,0 +1,12 @@
+//DOM
+//Document Object Model
+//Modelo de objeto del documento
+
+//console.log(documet.getElementById('tituloNombre));
+//console.log(document.getElementsByClassName('clase));
+
+console.log(document.getElementsByTagName("input"));
+console.log(document.getElementsByTagName("form"));
+console.log(document.getElementsByTagName("br"));
+console.log(document.getElementsByTagName("script"));
+console.log(document.getElementsByTagName("label"));
